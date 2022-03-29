ad30eaf3591d3f915cefad8ae3d86fee26b0e726
LynxEtagere <lucas.baret@lacatholille.fr>
2022-03-29 15:43:29 +0200

    c

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from '@vue/runtime-core'
+import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent({
+  components: {HelloWorld},
+  computed:{},
+  methods:{
+
+  }
+})
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/fgo.png" />
+  <HelloWorld/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+import { defineComponent } from 'vue';
+//coucou le pull
+export default defineComponent({
+    data(){
+      return{
+        characters:[]
+      }
+    },
+    mounted(){
+      axios.get("https://api.atlasacademy.io/basic/NA/servant/search?lang=en&type=normal&flag=normal&rarity=5&rarity=4&className=saber&gender=female" ,{
+
+      }).then((resultats)=>{
+        this.characters.push(resultats.data)
+        console.log(resultats.data)
+      })
+    },
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="chara in characters" :key="chara" >
+      <li v-for="char in chara" :key="char">
+        {{char}}
+        </li>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
