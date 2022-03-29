f526eb749006719c8183b5d28b5655aa8b094fb6
LynxEtagere <lucas.baret@lacatholille.fr>
2022-03-29 16:19:14 +0200

    t

--- src/components/character.vue
+++ src/components/character.vue
@@ -1,13 +1,35 @@
+<template>
+
+<div class="perso">
+
+
+
+</div>
+
+</template>
+
 <script>
 
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    props{
-        type: Object{
-            
+    props:{
+        character:{
+            Type : Object,
         }
     }
 })
 
 </script>
+
+<style scoped>
+.calculator {
+  background-color: rgba(15, 26, 75, 0.911);
+  /* margin: 1em; */
+  padding: 1em;
+  width: 200px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
